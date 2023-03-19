89e522f12c04b165bbf32f68961f23e3d1725438
Fusibu <ollerexpositomartin@gmail.com>
2023-03-19 21:30:04 +0100

    Added Supabase

---
+++ lib/features/auth/services/auth_service_impl.dart
@@ -0,0 +1,19 @@
+import 'package:moexfilm/features/auth/services/auth_service.dart';
+
+class AuthServiceImpl implements AuthService {
+  @override
+  void login(String email, String password) {
+    // TODO: implement login
+  }
+
+  @override
+  void loginWithGoogle() {
+    // TODO: implement loginWithGoogle
+  }
+
+  @override
+  void register(String email, String password, String passwordConfirmed) {
+    // TODO: implement register
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:moexfilm/app.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() async {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: "https://veplbweymcfyesluztaz.supabase.co",
+    anonKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlcGxid2V5bWNmeWVzbHV6dGF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxOTYyNjUsImV4cCI6MTk4Mzc3MjI2NX0.FaU6PbOx7CoYGbxWC5qLKVABjX240tYh1HNuCfNUzNAeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlcGxid2V5bWNmeWVzbHV6dGF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxOTYyNjUsImV4cCI6MTk4Mzc3MjI2NX0.FaU6PbOx7CoYGbxWC5qLKVABjX240tYh1HNuCfNUzNA",
+  );
+
   runApp(MoexfilmApp());
 }
